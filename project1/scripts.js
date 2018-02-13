0452798d679a8a709b1a0dc1c61ef019ffdbf0ba
brelliott708 <brelliott@umass.edu>
2018-02-13 18:44:02 -0500

    Added broken + button to schedule page. It will work when classes are generated by python code with unique identifiers

--- project1/scripts.js
+++ project1/scripts.js
@@ -1,7 +1,11 @@
 $(function() {
-
+    //button used to hide the search criteria
     $('#searchToggleBTN').on('click', function() {
         $('.search-toggle').toggle(1000);
     });
+    $('.btn-results').on('click', function() {
+        $('.result-details').toggle(1000);
+    });
+
 
 });
