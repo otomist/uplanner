44c913439f2978d2203b4d96f5558ba2bba29471
brelliott708 <brelliott@umass.edu>
2018-03-12 20:52:02 -0400

    added functions to change + to - when showing details for results. Added more demos of adding to and deleting from the schedule

--- project1/scripts.js
+++ project1/scripts.js
@@ -13,11 +13,14 @@ $(function () {
             resPanel.style.maxHeight='25rem';
         }
     });
+    
     //Button for maximize/minimize result info
     $('#b1').on('click', function () {
         $('.r1').toggle(1000);
+        document.getElementById("b1").textContent = 
+            (document.getElementById("b1").textContent === "+") ? "-" : "+";
     });
-    //button for adding to scheduler
+    //Button for adding to scheduler
     document.getElementById("add1").onclick = function () {
         scheduler.addEvent({
             id:1,
@@ -47,13 +50,88 @@ $(function () {
 
     $('#b2').on('click', function () {
         $('.r2').toggle(1000);
+        document.getElementById("b2").textContent = 
+            (document.getElementById("b2").textContent === "+") ? "-" : "+";
     });
+    //Button for adding to scheduler
+    document.getElementById("add2").onclick = function () {
+        scheduler.addEvent({
+            id:4,
+            start_date: "02-01-2018 10:00",
+            end_date:   "02-01-2018 11:15",
+            text:       "CS121"
+        });
+        scheduler.addEvent({
+            id:5,
+            start_date: "04-01-2018 10:00",
+            end_date:   "04-01-2018 11:15",
+            text:       "CS121"
+        });
+    };
+    //button for deleting from scheduler
+    document.getElementById("del2").onclick = function () {
+        scheduler.deleteEvent(4);
+        scheduler.deleteEvent(5);
+    };
+
     $('#b3').on('click', function () {
         $('.r3').toggle(1000);
+        document.getElementById("b3").textContent = 
+            (document.getElementById("b3").textContent === "+") ? "-" : "+";
     });
+    //Button for adding to scheduler
+    document.getElementById("add3").onclick = function () {
+        scheduler.addEvent({
+            id:6,
+            start_date: "01-01-2018 09:00",
+            end_date:   "01-01-2018 09:50",
+            text:       "CS187"
+        });
+        scheduler.addEvent({
+            id:7,
+            start_date: "03-01-2018 09:00",
+            end_date:   "03-01-2018 09:50",
+            text:       "CS187"
+        });
+        scheduler.addEvent({
+            id:8,
+            start_date: "05-01-2018 09:00",
+            end_date:   "05-01-2018 09:50",
+            text:       "CS187"
+        });
+    };
+    //button for deleting from scheduler
+    document.getElementById("del3").onclick = function () {
+        scheduler.deleteEvent(6);
+        scheduler.deleteEvent(7);
+        scheduler.deleteEvent(8);
+    };
+
     $('#b4').on('click', function () {
         $('.r4').toggle(1000);
+        document.getElementById("b4").textContent = 
+            (document.getElementById("b4").textContent === "+") ? "-" : "+";
     });
+    //Button for adding to scheduler
+    document.getElementById("add4").onclick = function () {
+        scheduler.addEvent({
+            id:9,
+            start_date: "02-01-2018 13:00",
+            end_date:   "02-01-2018 14:15",
+            text:       "CS187"
+        });
+        scheduler.addEvent({
+            id:10,
+            start_date: "04-01-2018 13:00",
+            end_date:   "04-01-2018 14:15",
+            text:       "CS187"
+        });
+    };
+    //button for deleting from scheduler
+    document.getElementById("del4").onclick = function () {
+        scheduler.deleteEvent(9);
+        scheduler.deleteEvent(10);
+    };
     
 
 });
