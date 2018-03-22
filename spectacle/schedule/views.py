b5245422adb76f9389a4a606729270a912b4ee1d
Jason Valladares <jasonvallada@umass.edu>
2018-03-22 18:26:02 -0400

    Added extensible block for body tags in html Fixed navbar highlighting with django variables

--- spectacle/schedule/views.py
+++ spectacle/schedule/views.py
@@ -8,35 +8,43 @@ def index(request):
     """
     View function for home page of site.
     """
+    highlight_index = True
     
     return render(
         request,
         'index.html',
-        context={}
+        context={'highlight_index':highlight_index}
     )
     
 def schedule(request):
     
     departments = Department.objects.all()
+    highlight_schedule = True
     
     return render(
         request,
         'schedule.html',
-        context={'departments':departments}
+        context={'departments':departments, 'highlight_schedule':highlight_schedule}
     )
     
 def userprofile(request):
     
+    highlight_profile = True
+    
     return render(
         request,
-        'userprofile.html'
+        'userprofile.html',
+        context={'highlight_profile':highlight_profile}
     )
     
 def flowchart(request):
+
+    highlight_prereqs = True
     
     return render(
         request,
-        'flowchart.html'
+        'flowchart.html',
+        context={'highlight_prereqs':highlight_prereqs}
     )
     
 class CourseDetailView(generic.DetailView):
