968851c3e211b9d2f83dbdbf978f0d153794cfdd
Jason Valladares <jasonvallada@umass.edu>
2018-03-27 13:28:26 -0400

    Minor database and form changes. Now the NULL department is default

--- spectacle/schedule/forms.py
+++ spectacle/schedule/forms.py
@@ -4,9 +4,9 @@ from .models import Course, Department
 class ScheduleForm(forms.Form):
     keywords = forms.CharField(required=False, help_text="Enter keywords to search for", max_length=200)
     
-    depts = map(lambda obj: (obj.code, obj.name), Department.objects.all())
-    #depts=['A', 'B']
-    departments = forms.TypedChoiceField(choices=depts, coerce=str, empty_value='', help_text="Enter department to search within")
+    depts = list(map(lambda obj: (obj.code, obj.name), Department.objects.all()))
+    depts = [('NULL','')] + depts
+    departments = forms.TypedChoiceField(choices=depts, coerce=str, initial='NULL', empty_value='NULL', help_text="Enter department to search within")
     
     # cleaning functions -- currently they do nothing
     def clean_keywords(self):
