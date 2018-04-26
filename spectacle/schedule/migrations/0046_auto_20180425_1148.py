8ddaf2438c69a18c1c14ecb287a631e7a075e8c2
Jason Valladares <jasonvallada@umass.edu>
2018-04-26 14:16:55 -0400

    migration issues fixed

--- spectacle/schedule/migrations/0046_auto_20180425_1148.py
+++ spectacle/schedule/migrations/0046_auto_20180425_1148.py
@@ -28,4 +28,9 @@ class Migration(migrations.Migration):
             name='color',
             field=models.CharField(choices=[('red', 'red'), ('#157ddf9f', 'blue'), ('green', 'green'), ('orange', 'orange'), ('purple', 'purple'), ('pink', 'pink'), ('brown', 'brown')], default=schedule.models.get_color, help_text='Enter the color for this course', max_length=15),
         ),
+        migrations.AlterField(
+            model_name='course',
+            name='session',
+            field=models.CharField(blank=True, choices=[('un', 'University'), ('uc', 'University Eligible/CPE'), ('ud', 'University Non-standard Dates'), ('ce', 'CPE Continuing Education'), ('cu', 'CPE Non-Standard Dates'), ('c1', 'CPE Summer Session 1'), ('c2', 'CPE Summer Session 2'), ('c3', 'CPE Summer Session 3')], default='u', help_text='The course career', max_length=2),
+        ),
     ]
