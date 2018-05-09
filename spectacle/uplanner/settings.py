c8b5f4592b2a4fd90e4e3d1564197d044b1ef142
Jason Valladares <jasonvallada@umass.edu>
2018-05-09 19:56:55 -0400

    Changed database to postgres. Lost all data in process

--- spectacle/uplanner/settings.py
+++ spectacle/uplanner/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'uplanner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'uplannerdata',
+        'USER': 'spectacle',
+        'PASSWORD': 'Melodie0f-t',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
